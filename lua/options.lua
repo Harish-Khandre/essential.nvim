f38e8f45477d5ae2c5017685fedbd2649cbad056
Harish-Khandre <harishkhandre2901@gmail.com>
2023-12-24 14:07:21 +0530

    new keymaps and bufferline

--- lua/options.lua
+++ lua/options.lua
@@ -5,23 +5,38 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
--- vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 
+vim.opt.incsearch = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
 
 vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.linebreak = true
 vim.opt.wrapmargin = 2
+
 vim.opt.smartindent = true
 vim.opt.cursorline = true
+vim.opt.spelllang = { "en" }
+vim.opt.spell = true
 
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr><esc>", { desc = "Delete buffer and escape" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,10 +10,19 @@ return {
 			vim.keymap.set("n", "<leader><space>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fp", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
-			vim.keymap.set("n", "<leader>,", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[ ] Find existing buffers" })
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>gI", builtin.lsp_implementations, { desc = "[G]oto [I]mplementation" })
+			vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, { desc = "[G]oto [I]mplementation" })
+			vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "[G]oto [I]mplementation" })
+			vim.keymap.set(
+				"n",
+				"<leader>,",
+				"<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
+				{ desc = "switch buffer" }
+			)
 			vim.keymap.set("n", "<leader>/", function()
 				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 					winblend = 10,
