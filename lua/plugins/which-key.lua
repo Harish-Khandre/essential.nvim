da4fc2591b628dd764dcd44a340e0c66ab78e6e0
Harish-Khandre <harishkhandre2901@gmail.com>
2023-12-27 01:38:13 +0530

    toggleterm and telescope changes

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -11,7 +11,6 @@ return {
 			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
 			["<leader>g"] = { name = "[G]o To", _ = "which_key_ignore" },
 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
 			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
 		})
 	end,
