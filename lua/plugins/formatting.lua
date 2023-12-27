fce352fd03b4af1ebf14e6008b8f9204f4900e15
Harish-Khandre <harishkhandre2901@gmail.com>
2023-12-27 21:31:13 +0530

    modified some plugins

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -34,6 +34,6 @@ return {
 				async = false,
 				timeout_ms = 1000,
 			})
-		end, { desc = "Format file or range (in visual mode)" })
+		end, { desc = "[F]ormat file or range (in visual mode)" })
 	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,21 +7,21 @@ return {
 		},
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader><space>", builtin.find_files, { desc = "Find files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep files" })
+			vim.keymap.set("n", "<leader><space>", builtin.find_files, { desc = "[F]ind files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[L]ive grep files" })
 			vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind existing buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[Find] current [W]ord" })
 			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
-			vim.keymap.set("n", "<leader>gI", builtin.lsp_implementations, { desc = "[G]oto [I]mplementation" })
-			vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, { desc = "[G]oto [I]mplementation" })
-			vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "[G]oto [I]mplementation" })
+			vim.keymap.set("n", "<leader>gI", builtin.lsp_implementations, { desc = "[G]o to [I]mplementation" })
+			vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, { desc = "[G]o to [D]efinitions" })
+			vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "[G]o to [R]eferences" })
 			vim.keymap.set(
 				"n",
 				"<leader>,",
 				"<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
-				{ desc = "switch buffer" }
+				{ desc = "[S]witch buffer" }
 			)
 			vim.keymap.set("n", "<leader>/", function()
 				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
