f62ad8c3a63da2997a271f62b2effe6b2c1f8ced
Harish-Khandre <harishkhandre2901@gmail.com>
2023-12-28 14:02:30 +0530

    show hidden files in telescope and neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,16 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle<CR>", { desc = "[E]xplorer" })
+		require("neo-tree").setup({
+			window = {
+				width = 25,
+			},
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		})
 	end,
 }
